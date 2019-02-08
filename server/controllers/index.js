226abf53ffa513f8352100d290397bdadc9cb6be
Alyssa Cipriano <alyssacipriano@gmail.com>
2019-02-07 21:39:37 -0800

    Update code to pass Pomander

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,34 +2,34 @@ var models = require('../models');
 
 module.exports = {
   messages: {
+    // a function which handles a get request for all messages
     get: function (req, res) {
-      console.log("------------------------get messages request is:");
-    }, // a function which handles a get request for all messages
+      console.log('------------------------get messages request is:');
+    },
+    // a function which handles posting a message to the database
     post: function (req, res) {
-      console.log("------------------------post messages request is:", req.body);
+      console.log('------------------------post messages request is:', req.body);
       var newMessage = req.body;
-      models.messages.post(newMessage, function(data){
+      models.messages.post(newMessage, function(data) {
         console.log(data);
         res.send(data);
-      })
-
-    } // a function which handles posting a message to the database
-
+      });
+    }
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-      console.log("------------------------get user request is:");
+      console.log('------------------------get user request is:');
     },
-    post: function (req, res) {
 
-      console.log("------------------------ post user request is:", req.body);
-      var newUser= req.body;
-      models.users.post(newUser, function(data){
+    post: function (req, res) {
+      console.log('------------------------ post user request is:', req.body);
+      var newUser = req.body;
+      models.users.post(newUser, function(data) {
         console.log(data);
         res.send(data);
-      })
+      });
     }
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,50 +1,49 @@
 var db = require('../db');
 var connection = db.dbConnection;
 connection.connect();
+
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function (message, cb) {
+    // a function which produces all the messages
+    get: function () {},
 
-      var userQuery = 'select id from users where name ="' + message.username +'"';
+    // a function which can be used to insert a message into the database
+    post: function (message, cb) {
+      var userQuery = 'select id from users where name ="' + message.username + '"';
       var queryArgs = [];
+
       connection.query(userQuery, queryArgs, function(err, results) {
         if (err) {
-          console.log( "ERROR on first query", userQuery);
+          console.log( 'ERROR on first query', userQuery);
           throw err;
         }
         var userId = results[0].id;
-        var queryString = 'insert into messages (text, user_id, roomname) values ("'+
-        message.message+'",'+userId+',"'+message.roomname+'")';
-        console.log("-------QUERYSTRING:", queryString);
+        var queryString = 'insert into messages (text, user_id, roomname) values ("' +
+        message.message + '",' + userId + ',"' + message.roomname + '")';
+
+        console.log('-------QUERYSTRING:', queryString);
         connection.query(queryString, queryArgs, function(err) {
           if (err) { throw err; }
           cb('Message inserted');
-        })
-
+        });
       });
-
-
-
-      /* username: 'Valjean',
+    }
+  },
+  /* username: 'Valjean',
   message: 'In mercy\'s name, three days is all I need.',
   roomname: 'Hello'  */
-
-    } // a function which can be used to insert a message into the database
-  },
 
   users: {
     // Ditto as above.
     get: function () {},
     post: function (user, cb) {
-
-      var queryString = 'insert into users (name) values ("'+ user.username+'")';
+      var queryString = 'insert into users (name) values ("' + user.username + '")';
       var queryArgs = [];
+
       connection.query(queryString, queryArgs, function(err) {
         if (err) { throw err; }
         cb('User inserted');
-      })
-
+      });
     }
   }
 };
